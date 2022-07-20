d4a2ca657aae70c683f879ff0ecf4ec6e88ca2af
Victor <victorreis222@gmail.com>
2022-07-20 18:12:41 -0300

    Exercício de fixação switch case

--- fundamentos/bloco-4-introducao-ao-javascript-e-logica-de-programacao/4-01-introducao-ao-javascript/scripts.js
+++ fundamentos/bloco-4-introducao-ao-javascript-e-logica-de-programacao/4-01-introducao-ao-javascript/scripts.js
@@ -1,8 +1,15 @@
-let weekDay = "sábado"
+let candidato = "Aprovada";
 
-if (weekDay === "segunda-feira" || weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
-    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
-}
-else {
-    console.log ("FINALMENTE, um descanso merecido UwU")
+switch (candidato) {
+    case "Aprovada":
+        console.log ("aprovada");
+        break;
+    case "lista":
+        console.log ("lista");
+        break;
+    case "Reprovada":
+        console.log ("reprovada");
+        break;
+    default:
+        console.log ("Não se aplica");
 }
