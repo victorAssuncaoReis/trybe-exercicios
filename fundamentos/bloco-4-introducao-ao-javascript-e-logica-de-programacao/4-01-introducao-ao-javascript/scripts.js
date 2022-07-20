6d136c0a46fef4d74be198cb41ac85f163b6fb85
Victor <victorreis222@gmail.com>
2022-07-20 15:18:38 -0300

    Primeiro exercício de fixação

---
+++ fundamentos/bloco-4-introducao-ao-javascript-e-logica-de-programacao/4-01-introducao-ao-javascript/scripts.js
@@ -0,0 +1,14 @@
+const myName = "Victor";
+
+const birthCity = "Mogi Mirim";
+
+let birthYear = 1996;
+
+birthYear = 2000;
+
+
+console.log(myName);
+
+console.log (birthYear);
+
+console.log (birthCity);
