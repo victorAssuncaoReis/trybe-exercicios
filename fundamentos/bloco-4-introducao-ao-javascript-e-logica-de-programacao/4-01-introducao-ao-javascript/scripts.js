07c9111ef2e4bcedfb0b93cd06b5d9b6bfd2e0fd
Victor <victorreis222@gmail.com>
2022-07-20 16:23:01 -0300

    Exercício de fixação operador AND ||

--- fundamentos/bloco-4-introducao-ao-javascript-e-logica-de-programacao/4-01-introducao-ao-javascript/scripts.js
+++ fundamentos/bloco-4-introducao-ao-javascript-e-logica-de-programacao/4-01-introducao-ao-javascript/scripts.js
@@ -1,20 +1,8 @@
-const currentHour = 16.05;
+let weekDay = "sábado"
 
-let message = "";
-
-if (currentHour >= 22) {
-    message = "Não deveríamos comer nada, é hora de dormir";
+if (weekDay === "segunda-feira" || weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
 }
-else if (currentHour >= 18 && currentHour < 22) {
-    message = "Rango da noite, vamos jantar :D";
+else {
+    console.log ("FINALMENTE, um descanso merecido UwU")
 }
-else if (currentHour >= 14 && currentHour < 18) {
-    message = "Vamos fazer um bolo pro café da tarde?";
-}
-else if (currentHour < 14 && currentHour > 11) {
-    message = "Hora do almoço!!!";
-}
-else if (currentHour > 4 && currentHour < 11) {
-    message = "Hmmm, cheiro de café recém passado";
-}
-console.log (message)
