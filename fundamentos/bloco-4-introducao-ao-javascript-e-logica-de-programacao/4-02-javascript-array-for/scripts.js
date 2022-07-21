c51134d0a62a33ae1a278acb52329077a27b3811
Victor <victorreis222@gmail.com>
2022-07-21 17:38:42 -0300

    Exercicio 2

--- fundamentos/bloco-4-introducao-ao-javascript-e-logica-de-programacao/4-02-javascript-array-for/scripts.js
+++ fundamentos/bloco-4-introducao-ao-javascript-e-logica-de-programacao/4-02-javascript-array-for/scripts.js
@@ -1,7 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
 
-for (index = 0; index < numbers.length; index += 1) {
-    console.log(numbers[index]);
+for (let index = 0; index < numbers.length; index +=1) {
+    sum += numbers[index];
 }
+console.log(sum);
+
+
   
 
