4625667e4492c08fc196c9c634f93dc78f21e9e0
Victor <victorreis222@gmail.com>
2022-07-21 18:38:53 -0300

    Exercício 6

--- fundamentos/bloco-4-introducao-ao-javascript-e-logica-de-programacao/4-02-javascript-array-for/scripts.js
+++ fundamentos/bloco-4-introducao-ao-javascript-e-logica-de-programacao/4-02-javascript-array-for/scripts.js
@@ -1,11 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let largest = numbers[0];
+let odds = 0;
 
-for (let index = 0; index < numbers.length; index +=1) {
-    if (largest < numbers[index] ) {
-        largest = numbers[index];
+for (index = 0; index < numbers.length; index +=1) {
+     if (numbers[index] % 2 !== 0) {
+        odds +=1;
     }
 }
-console.log(largest);
-  
+
+if (odds > 1) {
+    console.log(odds);
+} else {
+    console.log("Nenhum valor ímpar encontrado");
+}
+   
 
