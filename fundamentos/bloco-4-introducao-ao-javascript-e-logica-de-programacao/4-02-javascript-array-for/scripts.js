6de1c3fa14971133a3879084f2189cb1cf427965
Victor <victorreis222@gmail.com>
2022-07-21 18:01:24 -0300

    Exercício 5

--- fundamentos/bloco-4-introducao-ao-javascript-e-logica-de-programacao/4-02-javascript-array-for/scripts.js
+++ fundamentos/bloco-4-introducao-ao-javascript-e-logica-de-programacao/4-02-javascript-array-for/scripts.js
@@ -1,17 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
+let largest = numbers[0];
 
 for (let index = 0; index < numbers.length; index +=1) {
-    sum += numbers[index];
+    if (largest < numbers[index] ) {
+        largest = numbers[index];
+    }
 }
-console.log(sum/numbers.length);
-
-if (sum/numbers.length >= 20) {
-    console.log("Valor maior do que 20");
-} else {
-    console.log("Valor menor do que 20");
-}
-
-
+console.log(largest);
   
 
