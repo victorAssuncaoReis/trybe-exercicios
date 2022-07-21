b5e8d4e1acf96b118ae240ff9bb17644610b6989
Victor <victorreis222@gmail.com>
2022-07-21 18:42:56 -0300

    Exercício 7

--- fundamentos/bloco-4-introducao-ao-javascript-e-logica-de-programacao/4-02-javascript-array-for/scripts.js
+++ fundamentos/bloco-4-introducao-ao-javascript-e-logica-de-programacao/4-02-javascript-array-for/scripts.js
@@ -1,16 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let odds = 0;
+let sum = 0;
+let lowest = numbers[0];
 
-for (index = 0; index < numbers.length; index +=1) {
-     if (numbers[index] % 2 !== 0) {
-        odds +=1;
+for (let index = 0; index < numbers.length; index +=1) {
+    sum += numbers[index];
+    if (lowest > numbers[index] ) {
+        lowest = numbers[index];
     }
 }
 
-if (odds > 1) {
-    console.log(odds);
-} else {
-    console.log("Nenhum valor ímpar encontrado");
-}
+console.log(lowest)
    
 
