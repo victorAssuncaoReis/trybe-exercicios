fe9b28cd251e72c0922854f6295ae6c5b64cd6dc
Victor <victorreis222@gmail.com>
2022-07-22 19:04:17 -0300

    Último exercício

--- fundamentos/bloco-4-introducao-ao-javascript/4-03-logica-de-programacao/scripts.js
+++ fundamentos/bloco-4-introducao-ao-javascript/4-03-logica-de-programacao/scripts.js
@@ -1,7 +1,15 @@
-let n = 5;
+let biggestPrimeNumber = 0;
 
-for (index = 0; index <= n; index += 1) {
-     console.log("*".repeat(index));
+for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
 }
 
-
+console.log(biggestPrimeNumber);
