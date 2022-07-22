4ad19a0b08ceb5fcc25b0653afab94d2facf3473
Victor <victorreis222@gmail.com>
2022-07-22 16:18:24 -0300

    Exercício 3

--- fundamentos/bloco-4-introducao-ao-javascript/4-03-logica-de-programacao/scripts.js
+++ fundamentos/bloco-4-introducao-ao-javascript/4-03-logica-de-programacao/scripts.js
@@ -1,4 +1,7 @@
-let word = "Neymar";
+let array = ['java', 'javascript', 'python', 'html', 'css'];
 
-console.log(word.split("").reverse().join(""));
+array.sort((a,b) => a.length - b.length);
 
+console.log(array[array.length - 1]);
+
+console.log(array[0]);
