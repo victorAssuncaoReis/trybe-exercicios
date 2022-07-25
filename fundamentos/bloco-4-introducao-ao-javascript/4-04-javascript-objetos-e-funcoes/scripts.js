6504a86aeb32e3197224b9596fd1909c2ef343f0
Victor <victorreis222@gmail.com>
2022-07-25 19:02:38 -0300

    Exercício 3

--- fundamentos/bloco-4-introducao-ao-javascript/4-04-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-ao-javascript/4-04-javascript-objetos-e-funcoes/scripts.js
@@ -2,10 +2,11 @@
 function array(numeros) {
     let indexMaior = 0;
     for (let index in numeros) {
-        if (indexMaior < numeros[index]) {
-            indexMaior = numeros[index];
+        if (numeros[indexMaior] > numeros[index]) {
+            indexMaior = index;
         } 
     }
     return indexMaior;
 }
-console.log(array([2, 3, 6, 7, 10, 1]));
+console.log(array([2, 4, 6, 7, 10, 0, -3]));
+
