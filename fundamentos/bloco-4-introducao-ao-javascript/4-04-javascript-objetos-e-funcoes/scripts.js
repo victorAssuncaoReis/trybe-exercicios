0f53672fa0e17613e6c6e4741f9adffa441d8130
Victor <victorreis222@gmail.com>
2022-07-25 18:22:00 -0300

    Exercício 6

--- fundamentos/bloco-4-introducao-ao-javascript/4-04-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-ao-javascript/4-04-javascript-objetos-e-funcoes/scripts.js
@@ -1,24 +1,16 @@
- // Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
- let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
   };
 
-  info['recorrente'] = "Sim";
-
-  let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: "Sim",
-  };
-
-  for (let chaves in info) {
-   if (chaves === "recorrente" && info.recorrente === "Sim" && info2.recorrente === "Sim") {
-    console.log("Ambos recorrentes")
-   } else {
-    console.log(info[chaves] + " e " + info2[chaves]);
-   }
-}
+  console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + "'" + leitor['livrosFavoritos'][0]['titulo'] + ".'")
 
