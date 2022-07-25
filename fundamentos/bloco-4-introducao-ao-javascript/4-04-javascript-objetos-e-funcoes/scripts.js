b06d0674e1ef4cedd6b0244cf6beb493c633f143
Victor <victorreis222@gmail.com>
2022-07-25 16:00:40 -0300

    Exercício de fixação 2 for/in

--- fundamentos/bloco-4-introducao-ao-javascript/4-04-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-ao-javascript/4-04-javascript-objetos-e-funcoes/scripts.js
@@ -1,9 +1,9 @@
-let names = {
-    person1: 'João',
-    person2: 'Maria',
-    person3: 'Jorge',
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
   };
 
-  for (let index in names) {
-    console.log("Olá " + names[index]);
+  for (let index in car) {
+    console.log(index, car[index]);
   }
