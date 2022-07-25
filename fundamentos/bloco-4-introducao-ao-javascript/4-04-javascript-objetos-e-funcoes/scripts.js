8d9b72054083d9ef86157c44e2fba28abf1e46d2
Victor <victorreis222@gmail.com>
2022-07-25 18:38:07 -0300

    Exercício 01 functions

--- fundamentos/bloco-4-introducao-ao-javascript/4-04-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-ao-javascript/4-04-javascript-objetos-e-funcoes/scripts.js
@@ -1,25 +1,10 @@
-//Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
-let leitor = {
-    nome: 'Julia',
-    sobrenome: 'Pessoa',
-    idade: 21,
-    livrosFavoritos: [
-      {
-        titulo: 'O Pior Dia de Todos',
-        autor: 'Daniela Kopsch',
-        editora: 'Tordesilhas',
-      },
-    ],
-  };
-
-  console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + "'" + leitor['livrosFavoritos'][0]['titulo'] + ".'")
-
-  leitor.livrosFavoritos.push(
-    {
-      titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-      autor: 'JK Rowling',
-      editor: 'Rocco',
-    },
-  );
-
-  console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos");
+function palindromo(palavra) {
+    let reverse = palavra.split("").reverse().join("");
+    if (reverse === palavra) {
+        return true;
+    } else {
+        return false;
+    }
+    }
+    console.log(palindromo('arara'));
+    console.log(palindromo('vasco'));
