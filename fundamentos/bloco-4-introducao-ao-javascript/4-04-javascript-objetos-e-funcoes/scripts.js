a0903c7e32cad662cc7c091262e27e41ba955061
Victor <victorreis222@gmail.com>
2022-07-25 15:57:33 -0300

    Exercício de fixação 1 for/in

--- fundamentos/bloco-4-introducao-ao-javascript/4-04-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-ao-javascript/4-04-javascript-objetos-e-funcoes/scripts.js
@@ -1,13 +1,9 @@
-let player = {
-    name: "Marta",
-    lastName: "Silva",
-    age: 34,
-    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
-    medals: {
-        golden: 2,
-        silver: 3,
-    } 
-};
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
 
-console.log("A jogadora " + player.name + " " + player.lastName + " foi eleita a melhor jogadora do mundo " + player.bestInTheWorld.length + " vezes.");
-console.log("A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " de prata.");
+  for (let index in names) {
+    console.log("Olá " + names[index]);
+  }
