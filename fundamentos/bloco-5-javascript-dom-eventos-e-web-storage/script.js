85a2e8b08727758226c1aef8a4ef989cbdb9e121
Victor <victorreis222@gmail.com>
2022-07-27 18:24:56 -0300

    Exercícios do dia 5.01

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/script.js
@@ -0,0 +1,8 @@
+//document.getElementsByTagName("section h3").style.backgroundColor = "purple";
+document.getElementsByTagName("header")[0].style.backgroundColor = "green";
+document.getElementsByTagName("section")[0].style.backgroundColor = "orange";
+document.getElementsByTagName("section")[1].style.backgroundColor = "yellow";
+document.getElementsByTagName("h3")[0].style.backgroundColor = "purple";
+document.getElementsByTagName("h3")[1].style.backgroundColor = "purple";
+document.getElementsByTagName("h3")[2].style.backgroundColor = "black";
+document.getElementsByTagName("h3")[3].style.backgroundColor = "black";
