7393cd30c73fe412d125932f093258d614e21c79
Victor <victorreis222@gmail.com>
2022-08-12 17:01:57 -0300

    Exercício 01

---
+++ fundamentos/bloco-7-introducao-ao-javascript-es-6-e-testes-unitarios/scripts.js
@@ -0,0 +1,24 @@
+function testingScope(escopo) {
+    if (escopo === true) {
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      console.log(ifScope);
+    } else {
+      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+  }
+
+  testingScope(true);
+
+const testingScope = (escopo) => {
+    if (escopo ===true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo!`;
+    console.log(ifScope);
+} else {
+    let elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+    console.log(elseScope);
+}
+}
+testingScope(true);
