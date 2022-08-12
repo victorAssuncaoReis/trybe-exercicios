889bdb1ddb2d8ac837755d255eb357a7223b5873
Victor <victorreis222@gmail.com>
2022-08-12 17:35:08 -0300

    Exercício 03

--- fundamentos/bloco-7-introducao-ao-javascript-es-6-e-testes-unitarios/scripts.js
+++ fundamentos/bloco-7-introducao-ao-javascript-es-6-e-testes-unitarios/scripts.js
@@ -1,27 +1,8 @@
-/* function testingScope(escopo) {
-    if (escopo === true) {
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-      console.log(ifScope);
-    } else {
-      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope);
+const fatorial = (number) => {
+    let resultado = 1
+    for (index = 2; index <= number; index += 1) {
+        resultado *= index
     }
-  }
-
-  testingScope(true); */
-
-/* const testingScope = (escopo) => {
-    if (escopo ===true) {
-    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo!`;
-    console.log(ifScope);
-} else {
-    const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
-    console.log(elseScope);
+    return resultado
 }
-}
-testingScope(true); */
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-
-console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente!`);
+console.log(`Esse é o fatorial ${fatorial(5)}`);
