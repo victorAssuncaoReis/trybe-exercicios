54048ab5112afca0b10fb9658d9710e78a7fd7c2
Victor <victorreis222@gmail.com>
2022-08-12 17:20:32 -0300

    Exercício 02

--- fundamentos/bloco-7-introducao-ao-javascript-es-6-e-testes-unitarios/scripts.js
+++ fundamentos/bloco-7-introducao-ao-javascript-es-6-e-testes-unitarios/scripts.js
@@ -1,4 +1,4 @@
-function testingScope(escopo) {
+/* function testingScope(escopo) {
     if (escopo === true) {
       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
@@ -9,16 +9,19 @@ function testingScope(escopo) {
     }
   }
 
-  testingScope(true);
+  testingScope(true); */
 
-const testingScope = (escopo) => {
+/* const testingScope = (escopo) => {
     if (escopo ===true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
     ifScope = `${ifScope} ótimo, fui utilizada no escopo!`;
     console.log(ifScope);
 } else {
-    let elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+    const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
     console.log(elseScope);
 }
 }
-testingScope(true);
+testingScope(true); */
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente!`);
