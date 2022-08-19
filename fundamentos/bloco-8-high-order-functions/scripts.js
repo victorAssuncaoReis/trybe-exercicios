d9e07ca4831193c2da3f8a6e6815348650c0d0c6
Victor <victorreis222@gmail.com>
2022-08-19 19:05:37 -0300

    Exercício 03

--- fundamentos/bloco-8-high-order-functions/scripts.js
+++ fundamentos/bloco-8-high-order-functions/scripts.js
@@ -1,10 +1,76 @@
-const checagem = (numero, aleatorio) => numero === aleatorio;
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+const compareAnswers = (rightAnswer, studentAnswer) => {
+  if (rightAnswer === studentAnswer) {
+    return 1;
+  } if (studentAnswer === 'N.A') {
+    return 0;
+  }
+  return -0.5;
+};
+const countPoints = (rightAnswers, studentAnswers, action) => {
+  let counter = 0;
+  for (let index = 0; index < rightAnswers.length; index += 1) {
+    const actionReturn = action(rightAnswers[index], studentAnswers[index]);
+    counter += actionReturn;
+  }
+  return `Resultado final: ${counter} pontos`;
+};
 
-const sorteio = (numero, callback) => {
-  const aleatorio = Math.round(Math.random() *5);
-  return callback(numero, aleatorio) ? 'Parabéns você ganhou' : 'Tente novamente';
-}
+console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
 
-console.log(sorteio(3, checagem));
+const dragonAttack = (dragon) => {
+  const minDmg = 15;
+  const maxDmg = Math.floor(Math.random() * dragon.strength);
+};
 
+const warriorAttack = (warrior) => {
+  const minDmg = warrior.strength;
+  const maxDmg = Math.floor(Math.random() *(minDmg * warrior.weaponDmg));
+  const warriorDmg = maxDmg > minDmg ? maxDmg : minDmg;
+  return warriorDmg;
+};
+const mageAttack = (mage) => {
+  const mageMana = mage.mana;
+  const minDmg = mage.intelligence;
+  const maxDmg = minDmg * 2;
+  const turnStats = {
+    manaSpent: 0,
+    damageDealt: 'Not enough mana...',
+  };
 
+  if (mageMana > 15) {
+    mageDmg = minDmg < maxDmg ? maxDmg : minDmg
+    turnStats.manaSpent = 15;
+    turnStats.damageDealt = mageDamage;
+    return turnStats;
+  }
+  return turnStats;
+};
+
+const gameActions = {
+  warriorTurn: (warriorAttack) => {
+    const warriorDamage = warriorAttack(warrior);
+    warrior.damage = warriorDamage;
+    dragon.healthPoints -= warriorDamage;
+  },
+  mageTurn: (mageAttack) => {
+    const mageTurnStats = mageAttack(mage);
+    const mageDamage = mageTurnStats.damageDealt;
+    mage.damage = mageDamage;
+    mage.mana -= mageTurnStats.manaSpent;
+    dragon.healthPoints -= mageDamage;
+  },
+  dragonTurn: (dragonAttack) => {
+    const dragonDamage = dragonAttack(dragon);
+    mage.healthPoints -= dragonDamage;
+    warrior.healthPoints -= dragonDamage;
+    dragon.damage = dragonDamage;
+  },
+  turnResults: () => battleMembers,
+};
+
+gameActions.warriorTurn(warriorAttack);
+gameActions.mageTurn(mageAttack);
+gameActions.dragonTurn(dragonAttack);
+console.log(gameActions.turnResults());
