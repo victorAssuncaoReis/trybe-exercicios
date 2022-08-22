a5cf04d66a52ebbbc96e059a7bc7e6f929aae984
Victor <victorreis222@gmail.com>
2022-08-22 18:44:34 -0300

    Exercícios

--- fundamentos/bloco-8-high-order-functions/scripts.js
+++ fundamentos/bloco-8-high-order-functions/scripts.js
@@ -1,22 +1,89 @@
-const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna', 'Antedeguemon'];
-
-const hasName = (arr, name) => {
-  return arr.some((nomes) => nomes === name);
-}
-
-console.log(hasName(names, 'Ana'));
-console.log(hasName(names, 'Antedeguemon'));
-
-const people = [
-  { name: 'Mateus', age: 18 },
-  { name: 'José', age: 19 },
-  { name: 'Ana', age: 23 },
-  { name: 'Cláudia', age: 20 },
-  { name: 'Bruna', age: 19 },
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
 ];
 
-const verifyAges = (arr, minimumAge) => {
-  return arr.every((idades) => idades.age >= minimumAge);
+// Adicione o código do exercício aqui:
+function authorBornIn1947() {
+  return console.log(books.find((book) => book.author.birthYear === 1947).author.name);
 }
+authorBornIn1947();
 
-console.log(verifyAges(people, 18));
+function getNamedBook() {
+  return books.find((book) => book.name.length === 26);
+}
+console.log(getNamedBook());
+
+const booksOrderedByReleaseYearDesc = () => books.sort((a, b) => b.releaseYear - a.releaseYear); 
+
+console.log(booksOrderedByReleaseYearDesc());
+
+
+
+const everyoneWasBornOnSecXX = () => books.every((books) => books.author.birthYear < 2000 && books.author.birthYear > 1901);
+
+console.log(everyoneWasBornOnSecXX());
+
+const expectedResult = true;
+const someBookWasReleaseOnThe80s = () => books.some((book) => book.releaseYear > 1980 && book.releaseYear < 1990);
+
+console.log(someBookWasReleaseOnThe80s());
+
