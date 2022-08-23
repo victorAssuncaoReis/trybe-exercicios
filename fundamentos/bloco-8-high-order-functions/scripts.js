deeebf334db5472e1494192ad512ba808a540aac
Victor <victorreis222@gmail.com>
2022-08-23 18:19:39 -0300

    Exercício 02

--- fundamentos/bloco-8-high-order-functions/scripts.js
+++ fundamentos/bloco-8-high-order-functions/scripts.js
@@ -62,20 +62,48 @@ const books = [
 ];
 
 const expectedResult = [
-  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
-  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
-  'Fundação - Ficção Científica - Isaac Asimov',
-  'Duna - Ficção Científica - Frank Herbert',
-  'A Coisa - Terror - Stephen King',
-  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
 ];
+
+function nameAndAge(books) {
+  return books.map((elementos) => {
+    return {
+      age: elementos.releaseYear - elementos.author.birthYear,
+      author: elementos.author.name,
+    }
+  }).sort((a, b) => a.age - b.age);
+}
+console.log(nameAndAge(books));
 // Adicione o código do exercício aqui:
 
-function formatedBookNames(books) {
+/* function formatedBookNames(books) {
  return books.map((elementos) => {
     return `${elementos.name} - ${elementos.genre} - ${elementos.author.name}`
   })
 }
-console.log(formatedBookNames(books));
+console.log(formatedBookNames(books)); */
 
-//Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+//🚀 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
