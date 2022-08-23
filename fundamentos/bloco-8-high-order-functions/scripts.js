ecf575eccd0f391af51a0432c657ea6f4718346d
Victor <victorreis222@gmail.com>
2022-08-23 18:35:09 -0300

    Exercício 04

--- fundamentos/bloco-8-high-order-functions/scripts.js
+++ fundamentos/bloco-8-high-order-functions/scripts.js
@@ -63,10 +63,36 @@ const books = [
 
 // Adicione o código do exercício aqui:
 
-const fantasyOrScienceFiction = (books) => books.filter((book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia');
+const expectedResult = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+];
 
-console.log(fantasyOrScienceFiction(books));
-
-
-/* 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
-Dica: use a função filter; */
+function oldBooksOrdered(books) {
+  return books.filter((book) => {
+    return 2022 - book.releaseYear > 60
+  }).sort((a, b) => (a.releaseYear - b.releaseYear));
+}
+console.log(oldBooksOrdered(books)); 
+/* 
+🚀 4 - Crie um array formado pelos livros com mais de 60 anos desde sua publicação. Esse array deve ser ordenado do livro mais velho ao mais novo.
+Dica: use as funções filter e sort */
