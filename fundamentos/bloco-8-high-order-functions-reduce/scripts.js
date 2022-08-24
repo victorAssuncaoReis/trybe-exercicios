acbc8fbab8f471d640ac05c637f7e820ee4eae8a
Victor <victorreis222@gmail.com>
2022-08-24 19:00:49 -0300

    Exercício 06

--- fundamentos/bloco-8-high-order-functions-reduce/scripts.js
+++ fundamentos/bloco-8-high-order-functions-reduce/scripts.js
@@ -1,15 +1,15 @@
-const names = [
-  'Aanemarie', 'Adervandes', 'Akifusa',
-  'Abegildo', 'Adicellia', 'Aladonata',
-  'Abeladerco', 'Adieidy', 'Alarucha',
-];
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
-function containsA() {
-  return names.reduce((acc, currWord) =>
-     acc + currWord.split('').reduce((acumulator, currLetter) => {
-        if (currLetter === 'a' || currLetter === 'A') return acumulator + 1;
-        return acumulator;
-     }, 0), 0);
+function studentAverage() {
+  return nameAndAverage = students.map((student, index) => (
+    {
+    name: student,
+    average: (grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length),
+  }
+  ));
 }
-
-console.log(containsA());
+console.log(studentAverage());
+// ...
+// ...
+/* 🚀 6- Para o próximo exercício você deve criar um novo array de objetos a partir das informações das constantes students e grades, onde cada objeto desse novo array terá o formato { name: nome do aluno, average: media das notas }. Para isso vamos assumir que a posição do index 0 de grades refere-se ao estudante na posição de index 0 de students. Para resolver este exercício, além de reduce, será necessário utilizar também a função map. Dica: Você pode acessar o index do array dentro de map, e você pode ver o objeto esperado na constante expected. */
