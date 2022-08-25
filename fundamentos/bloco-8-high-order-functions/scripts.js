ca7194ec9f46105493887285631edb44664ff29b
Victor <victorreis222@gmail.com>
2022-08-25 18:32:39 -0300

    Exercício 05

--- fundamentos/bloco-8-high-order-functions/scripts.js
+++ fundamentos/bloco-8-high-order-functions/scripts.js
@@ -1,39 +1,9 @@
+const myList = [5, 2, 3];
 
-const people = [
-  {
-    name: 'Nicole',
-    bornIn: 1992,
-    nationality: 'Australian',
-  },
-  {
-    name: 'Harry',
-    bornIn: 2008,
-    nationality: 'Australian',
-  },
-  {
-    name: 'Toby',
-    bornIn: 1901,
-    nationality: 'Australian',
-  },
-  {
-    name: 'Frida',
-    bornIn: 1960,
-    nationality: 'Dannish',
-  },
-  {
-    name: 'Fernando',
-    bornIn: 2001,
-    nationality: 'Brazilian',
-  },
-];
-//bornIn: nascido em
-
-// escreva filterPeople abaixo
-
-/* //🚀 4 - Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
-Dica: use object destructuring. */
-
-const filterPeople = () => people.filter(({nationality, bornIn}) =>  (nationality === 'Australian' && bornIn <= 2000));
-console.log(filterPeople());
-
+// escreva swap abaixo
+const swap = ([el1, el2, el3]) => ([el3, el2, el1]);
+console.log(swap(myList));
+/* 5 - Escreva a função swap, que dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando somente 1 linha. O retorno da função utilizando o array myList deve ser [3, 2, 5].
+Dica: use array destructuring.
+ */
 
