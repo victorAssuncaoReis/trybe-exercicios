34acb599c9bbd380dbb8bf62f1c08faf34b0cecb
Victor <victorreis222@gmail.com>
2022-08-25 18:41:47 -0300

    Exercício 08

--- fundamentos/bloco-8-high-order-functions/scripts.js
+++ fundamentos/bloco-8-high-order-functions/scripts.js
@@ -1,26 +1,5 @@
-const ships = [
-  {
-    name: 'Titanic',
-    length: 269.1,
-    measurementUnit: 'meters',
-  },
-  {
-    name: 'Queen Mary 2',
-    length: 1132,
-    measurementUnit: 'feet',
-  },
-  {
-    name: 'Yamato',
-    length: 256,
-    measurementUnit: 'meters',
-  },
-];
-// measurementUnit: unidade de medida
-const shipLength = ({name, length, measurementUnit}) => (`${name} is ${length} ${measurementUnit}`);
-// escreva shipLength abaixo
-/* 7 - Escreva uma função shipLength que, dado um objeto representando um navio, retorna o comprimento dele, mostrando também a devida unidade de medida:
-Dica: use object destructuring. */
-
-console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
-console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
-console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
+// escreva greet abaixo
+const greet = (name, greeting = 'Hi') => (`${greeting} ${name}`);
+console.log(greet('John')) // 'Hi John'
+console.log(greet('John', 'Good morning')) // 'Good morning John'
+console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
