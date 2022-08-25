3049e522f3f3c9a75b5f31d4844bc0bd69fdd83e
Victor <victorreis222@gmail.com>
2022-08-25 18:46:42 -0300

    Refatorando exercicio 09

--- fundamentos/bloco-8-high-order-functions/scripts.js
+++ fundamentos/bloco-8-high-order-functions/scripts.js
@@ -6,9 +6,7 @@ const yearSeasons = {
   winter: ['December', 'January', 'February'],
 };
 const {spring, summer, autumn, winter} = yearSeasons;
-const exercicio = (array) => {
-  return (array);
-}
+const exercicio = (array) => (array);
 console.log(...winter, ...spring, ...summer, ...autumn);
 /* 9 - Existe um objeto yearSeasons que representam as estações do ano. Cada estação é uma chave deste objeto e o valor de cada chave é um array de strings, representando os meses daquela estação. A partir deste objeto, desestruture as estações do ano e espalhe-as em um array de meses do ano.
 Dica: use object destructuring e spread operator.
