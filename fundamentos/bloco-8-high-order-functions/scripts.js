5695aec155a4c7e870e2e5e012a3fbf964c52e6f
Victor <victorreis222@gmail.com>
2022-08-25 18:20:55 -0300

    Exercício 03

--- fundamentos/bloco-8-high-order-functions/scripts.js
+++ fundamentos/bloco-8-high-order-functions/scripts.js
@@ -1,4 +1,26 @@
-const sum = (...par) => (par.reduce((acc, curr) => acc + curr));
+/* <!-- name: nome da pessoa, age: idade, likes: gosta de, nationality: nacionalidade --> */
 
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
 
-console.log(sum(4,5,6,5,300));
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+// complete a assinatura da função abaixo
+const personLikes = ({name, age, likes, nationality}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+console.log(personLikes(alex));
+console.log(personLikes(gunnar));
+/* <!-- <nome> tem <anos de idade> e gosta de <gostos da pessoa> -->
+
+<!-- Retornos esperados: -->
+console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.' */
