8ee17d3d7945eee918e70c0954edd4e2f49552b9
Victor <victorreis222@gmail.com>
2022-08-25 18:14:21 -0300

    Exercício 02

--- fundamentos/bloco-8-high-order-functions/scripts.js
+++ fundamentos/bloco-8-high-order-functions/scripts.js
@@ -1,10 +1,4 @@
-const rectangleArea = (width, height) => width * height;
+const sum = (...par) => (par.reduce((acc, curr) => acc + curr));
 
-const rectangle1 = [1, 2];
-const rectangle2 = [3, 5];
-const rectangle3 = [6, 9];
-const rectangles = [rectangle1, rectangle2, rectangle3];
 
-rectangles.forEach((rectangle) => {
-  console.log(rectangleArea(...rectangle)); // Altere o parâmetro recebido por rectangleArea()
-});
+console.log(sum(4,5,6,5,300));
