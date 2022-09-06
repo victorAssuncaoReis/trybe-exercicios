ade34466acc96728d8b9423abcfe3afa37f961ef
Victor <victorreis222@gmail.com>
2022-09-06 18:54:45 -0300

    Exercício 06

--- front-end/bloco-1-introducao-front-end/scripts.js
+++ front-end/bloco-1-introducao-front-end/scripts.js
@@ -3,23 +3,32 @@ const messageDelay = () => Math.floor(Math.random() * 5000);
 const getMarsTemperature = () => {
   const maxTemperature = 58;
   return Math.floor(Math.random() * maxTemperature);
-};
+}
 
 const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
 
 const temperatureInFahrenheit = (temperature) =>
-  console.log(`Atualmente está ${toFahrenheit(temperature)}ºF em Marte`);
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
 
 const greet = (temperature) =>
-  console.log(`Olá! Curiosity aqui. Nesse momento está ${temperature}ºC em Marte`);
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
-// Definição da função sendMarsTemperature...
+const handleError = (errorReason) =>
+  console.log(`Error getting temperature: ${errorReason}`);
 
-const sendMarsTemperature = (callback) => {
+// definição da função sendMarsTemperature...
+const sendMarsTemperature = (callback, error) => {
   const temperature = getMarsTemperature();
-  setTimeout(() => callback(temperature), messageDelay());
+  const messageSuccessfullySent = Math.random() <= 0.6;
+   setTimeout(() => { 
+    if (messageSuccessfullySent) callback(temperature)
+    else error('Robot is busy');
+  }, messageDelay());
 }
 
-sendMarsTemperature(temperatureInFahrenheit); // Imprime "Atualmente está 46.4ºF em Marte", por exemplo
 
-sendMarsTemperature(greet); // Imprime "Olá! Curiosity aqui. Nesse momento são 36ºC em Marte", por exemplo
+// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+
+// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(greet, handleError);
