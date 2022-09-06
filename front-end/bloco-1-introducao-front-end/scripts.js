c8f73830d83422e09b10dbf01f4eb57320573a41
Victor <victorreis222@gmail.com>
2022-09-06 19:00:47 -0300

    exercício 07

--- front-end/bloco-1-introducao-front-end/scripts.js
+++ front-end/bloco-1-introducao-front-end/scripts.js
@@ -1,34 +1,44 @@
-const messageDelay = () => Math.floor(Math.random() * 5000);
+const pokemons = [
+  {
+    name: 'Bulbasaur',
+    type: 'Grama',
+    ability: 'Raio Solar',
+  },
+  {
+    name: 'Charmander',
+    type: 'Fogo',
+    ability: 'Lança Chamas',
+  },
+  {
+    name: 'Squirtle',
+    type: 'Água',
+    ability: 'Jato de Água',
+  },
+];
 
-const getMarsTemperature = () => {
-  const maxTemperature = 58;
-  return Math.floor(Math.random() * maxTemperature);
+function getPokemonDetails(selectedPokemon, callback) {
+  const foundPokemon = pokemons.find((pokemon) => pokemon.name === selectedPokemon);
+
+  setTimeout(() => {
+    if (foundPokemon === undefined) {
+      return callback(new Error('Não temos esse pokémon para você :('), null);
+    }
+
+    const { name, type, ability } = foundPokemon;
+
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+
+    callback(null, messageFromProfOak);
+  }, 2000);
 }
 
-const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
+const handlePokemonSearch = (error, message) => {
+  // Implemente a callback de tratamento de erro
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(message);
+  }
+};
 
-const temperatureInFahrenheit = (temperature) =>
-  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
-
-const greet = (temperature) =>
-  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
-
-const handleError = (errorReason) =>
-  console.log(`Error getting temperature: ${errorReason}`);
-
-// definição da função sendMarsTemperature...
-const sendMarsTemperature = (callback, error) => {
-  const temperature = getMarsTemperature();
-  const messageSuccessfullySent = Math.random() <= 0.6;
-   setTimeout(() => { 
-    if (messageSuccessfullySent) callback(temperature)
-    else error('Robot is busy');
-  }, messageDelay());
-}
-
-
-// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
-sendMarsTemperature(temperatureInFahrenheit, handleError);
-
-// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
-sendMarsTemperature(greet, handleError);
+getPokemonDetails('Digimon', handlePokemonSearch);
